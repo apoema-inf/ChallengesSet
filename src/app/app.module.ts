499c475d456df799f706d3c113cdf2423195fb16
apoema-inf <apoema.inf@gmail.com>
2018-10-15 16:54:52 -0300

    primeira construção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ServicosComponent } from './servicos/servicos.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'servicos',
+    component: ServicosComponent,
+    data: { title: 'Serviços' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicosComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
